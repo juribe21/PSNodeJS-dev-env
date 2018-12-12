061fd995757507d23bcfbbdc16c5785f6117a37c
Jorge Uribe <Jorge.Uribe@mitechnologiesinc.com>
2018-12-12 07:48:30 -0800

    helloWord Add

---
+++ helloWorld.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trainings
+{
+    public static class Constants
+    {
+        public static string SellerId { get { return "SellerId"; } }
+        public static string Acceesskey { get { return "AccessKey"; } }
+        public static string SecretAccessKey { get { return "SecretAccessKey"; } }
+
+        public static string SecretAccessKeyVOne { get { return "SecretAccessKeyVone"; } }
+        public static string SecretAccessKeyVTwo { get { return "SecretAccessKeyVTwo"; } }
+    }
+}
